f2947a3aa303284b8b417efe43db954a51dc7507
vivlong <vivlonglz@gmail.com>
2019-09-03 10:57:21 +0800

    Add some functions follow official demo

--- src/Lite.php
+++ src/Lite.php
@@ -1,139 +1,358 @@
 <?php
+
 namespace PhalApi\AliyunOss;
 
 use OSS\OssClient;
 use OSS\Core\OssException;
 
-class Lite {
-
+class Lite
+{
     protected $config;
 
     protected $client;
 
-    public function __construct($config = NULL) {
+    public function __construct($config = null)
+    {
+        $di = \PhalApi\DI();
         $this->config = $config;
-        if ($this->config === NULL) {
-            $this->config = \PhalApi\DI()->config->get('app.AliyunOss');
+        if ($this->config === null) {
+            $this->config = $di->config->get('app.AliyunOss');
         }
-        $accessKeyId        = $this->config['accessKeyId'];
-        $accessKeySecret    = $this->config['accessKeySecret'];
-        $endpoint           = $this->config['endpoint'];
-        $isCName            = $this->config['isCName'];
-        $securityToken      = $this->config['securityToken'];
-        $requestProxy       = $this->config['requestProxy'];
+        $accessKeyId = $this->config['accessKeyId'];
+        $accessKeySecret = $this->config['accessKeySecret'];
+        $endpoint = $this->config['endpoint'];
+        $isCName = $this->config['isCName'];
+        $securityToken = $this->config['securityToken'];
+        $requestProxy = $this->config['requestProxy'];
         try {
             $ossClient = new OssClient($accessKeyId, $accessKeySecret, $endpoint, $isCName, $securityToken, $requestProxy);
             $ossClient->setTimeout(3600); // 设置Socket层传输数据的超时时间，单位秒，默认5184000秒。
             $ossClient->setConnectTimeout(10); // 设置建立连接的超时时间，单位秒，默认10秒。
             $this->client = $ossClient;
         } catch (OssException $e) {
-            \PhalApi\DI()->logger->error($e->getMessage());
+            $di->logger->error($e->getMessage());
         }
     }
 
-    public function getClient() {
+    public function getClient()
+    {
         return $this->client;
     }
 
-    public function getConfig() {
+    public function getConfig()
+    {
         return $this->config;
     }
 
     public function uploadFile($bucket, $object, $filePath, $options = null)
     {
+        $di = \PhalApi\DI();
         if (!file_exists($filePath)) {
-            \PhalApi\DI()->logger->error('AliyunOss \ file not exists', $filePath);
+            $di->logger->error('AliyunOss # uploadFile # file not exists', $filePath);
+
             return false;
         }
-        try{
+        if (!$this->client->doesBucketExist($bucket)) {
+            $di->logger->error('AliyunOss # uploadFile # Bucket not exists', $bucket);
+
+            return false;
+        }
+        try {
             $res = $this->client->uploadFile($bucket, $object, $filePath, $options);
+
             return $res;
-        } catch(OssException $e) {
-            \PhalApi\DI()->logger->error('AliyunOss \ uploadFile', $e->getMessage());
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # uploadFile', $e->getMessage());
+
             return false;
         }
     }
 
-    public function copyObject($bucket, $from_object, $to_object)
+    public function multiuploadFile($bucket, $object, $filePath, $options = null)
     {
-        if (!$this->client->doesObjectExist($bucket, $from_object)) {
-            \PhalApi\DI()->logger->error('AliyunOss \ file not exists', $from_object);
+        $di = \PhalApi\DI();
+        if (!file_exists($filePath)) {
+            $di->logger->error('AliyunOss # multiuploadFile # file not exists', $filePath);
+
             return false;
         }
-        try{
-            $res = $this->client->copyObject($bucket, $from_object, $object, $to_object);
+        if (!$this->client->doesBucketExist($bucket)) {
+            $di->logger->error('AliyunOss # multiuploadFile # Bucket not exists', $bucket);
+
+            return false;
+        }
+        if (empty($options)) {
+            $options = array(
+                OssClient::OSS_CHECK_MD5 => true,
+                OssClient::OSS_PART_SIZE => 1,
+            );
+        }
+        try {
+            $res = $this->client->multiuploadFile($bucket, $object, $filePath, $options);
+
             return $res;
-        } catch(OssException $e) {
-            \PhalApi\DI()->logger->error('AliyunOss \ copyObject', $e->getMessage());
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # multiuploadFile', $e->getMessage());
+
             return false;
         }
     }
 
-    public function uploadPartCopy($bucket, $src_object, $dst_object)
+    public function copyObject($fromBucket, $fromObject, $toBucket, $toObject)
     {
-        if (!$this->client->doesObjectExist($bucket, $src_object)) {
-            \PhalApi\DI()->logger->error('AliyunOss \ file not exists', $from_object);
+        $di = \PhalApi\DI();
+        if (!$this->client->doesBucketExist($fromBucket)) {
+            $di->logger->error('AliyunOss # copyObject # Bucket not exists', $fromBucket);
+
             return false;
         }
-        try{
-            // 初始化分片。
-            $upload_id = $this->client->initiateMultipartUpload($bucket, $dst_object);
+        if (!$this->client->doesObjectExist($fromBucket, $fromObject)) {
+            $di->logger->error('AliyunOss # copyObject # file not exists', $fromObject);
+
+            return false;
+        }
+        if (empty($toBucket)) {
+            $toBucket = $fromBucket;
+        } else {
+            if (!$this->client->doesBucketExist($toBucket)) {
+                $di->logger->error('AliyunOss # copyObject # dst Bucket not exists', $toBucket);
+
+                return false;
+            }
+        }
+        try {
+            $res = $this->client->copyObject($fromBucket, $fromObject, $toBucket, $toObject);
+
+            return $res;
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # copyObject', $e->getMessage());
+
+            return false;
+        }
+    }
+
+    public function uploadPartCopy($fromBucket, $fromObject, $toBucket, $toObject)
+    {
+        $di = \PhalApi\DI();
+        if (!$this->client->doesBucketExist($fromBucket)) {
+            $di->logger->error('AliyunOss # uploadPartCopy # Bucket not exists', $fromBucket);
+
+            return false;
+        }
+        if (!$this->client->doesObjectExist($fromBucket, $fromObject)) {
+            $di->logger->error('AliyunOss # uploadPartCopy # file not exists', $fromObject);
+
+            return false;
+        }
+        if (empty($toBucket)) {
+            $toBucket = $fromBucket;
+        } else {
+            if (!$this->client->doesBucketExist($toBucket)) {
+                $di->logger->error('AliyunOss # uploadPartCopy # dst Bucket not exists', $toBucket);
+
+                return false;
+            }
+        }
+        try {
+            // 初始化分片
+            $uploadId = $this->client->initiateMultipartUpload($toBucket, $toObject);
             $copyId = 1;
-            // 逐个分片拷贝。
-            $eTag = $this->client->uploadPartCopy($bucket, $src_object, $bucket, $dst_object, $copyId, $upload_id);
+            // 逐个分片拷贝
+            $eTag = $this->client->uploadPartCopy($fromBucket, $fromObject, $toBucket, $toObject, $copyId, $uploadId);
             $upload_parts[] = array(
                 'PartNumber' => $copyId,
                 'ETag' => $eTag,
             );
-            // 完成分片拷贝。
-            $result = $this->client->completeMultipartUpload($bucket, $dst_object, $upload_id, $upload_parts);
+            // 完成分片拷贝
+            $result = $this->client->completeMultipartUpload($toBucket, $toObject, $uploadId, $upload_parts);
+
             return $result;
-        } catch(OssException $e) {
-            \PhalApi\DI()->logger->error('AliyunOss \ uploadPartCopy', $e->getMessage());
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # uploadPartCopy', $e->getMessage());
+
+            return false;
+        }
+    }
+
+    public function uploadPart($bucket, $object, $filePath)
+    {
+        $di = \PhalApi\DI();
+        if (!file_exists($filePath)) {
+            $di->logger->error('AliyunOss # uploadPart # file not exists', $filePath);
+
+            return false;
+        }
+        if (!$this->client->doesBucketExist($bucket)) {
+            $di->logger->error('AliyunOss # uploadPart # Bucket not exists', $bucket);
+
+            return false;
+        }
+        try {
+            /**
+             *  步骤1：初始化一个分片上传事件，获取uploadId。
+             */
+            $uploadId = $this->client->initiateMultipartUpload($bucket, $object);
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # uploadPart # initiateMultipartUpload FAILED', $e->getMessage());
+
+            return false;
+        }
+        /*
+         * 步骤2：上传分片。
+         */
+        $partSize = 10 * 1024 * 1024;
+        $uploadFileSize = filesize($filePath);
+        $pieces = $this->client->generateMultiuploadParts($uploadFileSize, $partSize);
+        $responseUploadPart = array();
+        $uploadPosition = 0;
+        $isCheckMd5 = true;
+        foreach ($pieces as $i => $piece) {
+            $fromPos = $uploadPosition + (int) $piece[OssClient::OSS_SEEK_TO];
+            $toPos = (int) $piece[OssClient::OSS_LENGTH] + $fromPos - 1;
+            $upOptions = array(
+                OssClient::OSS_FILE_UPLOAD => $filePath,
+                OssClient::OSS_PART_NUM => ($i + 1),
+                OssClient::OSS_SEEK_TO => $fromPos,
+                OssClient::OSS_LENGTH => $toPos - $fromPos + 1,
+                OssClient::OSS_CHECK_MD5 => $isCheckMd5,
+            );
+            // MD5校验
+            if ($isCheckMd5) {
+                $contentMd5 = OssUtil::getMd5SumForFile($filePath, $fromPos, $toPos);
+                $upOptions[OssClient::OSS_CONTENT_MD5] = $contentMd5;
+            }
+            // 上传分片
+            try {
+                $responseUploadPart[] = $this->client->uploadPart($bucket, $object, $uploadId, $upOptions);
+            } catch (OssException $e) {
+                $di->logger->error("AliyunOss # uploadPart # part#{$i} FAILED", $e->getMessage());
+
+                return;
+            }
+        }
+        // $uploadParts是由每个分片的ETag和分片号（PartNumber）组成的数组。
+        $uploadParts = array();
+        foreach ($responseUploadPart as $i => $eTag) {
+            $uploadParts[] = array(
+                'PartNumber' => ($i + 1),
+                'ETag' => $eTag,
+            );
+        }
+        /*
+         * 步骤3：完成上传。
+         */
+        try {
+            // 在执行该操作时，需要提供所有有效的$uploadParts。OSS收到提交的$uploadParts后，会逐一验证每个分片的有效性。当所有的数据分片验证通过后，OSS将把这些分片组合成一个完整的文件。
+            $result = $this->client->completeMultipartUpload($bucket, $object, $uploadId, $uploadParts);
+
+            return $result;
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # uploadPart # completeMultipartUpload FAILED', $e->getMessage());
+
+            return false;
+        }
+    }
+
+    public function putObject($bucket, $object, $content, $options = null)
+    {
+        $di = \PhalApi\DI();
+        if (empty($content)) {
+            $di->logger->error('AliyunOss # putObject # content is empty');
+
+            return false;
+        }
+        if (!$this->client->doesBucketExist($bucket)) {
+            $di->logger->error('AliyunOss # putObject # Bucket not exists', $bucket);
+
+            return false;
+        }
+        try {
+            $res = $this->client->putObject($bucket, $object, $content, $options);
+
+            return $res;
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # putObject', $e->getMessage());
+
             return false;
         }
     }
 
     public function listObjects($bucket, $prefix = '', $delimiter = '/', $maxkeys = 100, $nextMarker = '')
     {
+        $di = \PhalApi\DI();
+        if (!$this->client->doesBucketExist($bucket)) {
+            $di->logger->error('AliyunOss # listObjects # Bucket not exists', $bucket);
+
+            return false;
+        }
         $options = array(
             'delimiter' => $delimiter,
             'prefix' => $prefix,
             'max-keys' => $maxkeys,
             'marker' => $nextMarker,
         );
-        try{
+        try {
             $listObjectInfo = $this->client->listObjects($bucket, $options);
-            $objectList = $listObjectInfo->getObjectList(); // object list
-            $prefixList = $listObjectInfo->getPrefixList(); // directory list
+            $objectList = $listObjectInfo->getObjectList();
+            $prefixList = $listObjectInfo->getPrefixList();
+            $objects = array();
+            $prefixs = array();
+            if (!empty($objectList)) {
+                foreach ($objectList as $objectInfo) {
+                    array_push($objects, $objectInfo->getKey());
+                }
+            }
+            if (!empty($prefixList)) {
+                foreach ($prefixList as $prefixInfo) {
+                    array_push($prefixs, $prefixInfo->getPrefix());
+                }
+            }
+
             return array(
-                'object' => $objectList,
-                'directory' => $prefixList
+                'object' => $objects,
+                'directory' => $prefixs,
             );
-        } catch(OssException $e) {
-            \PhalApi\DI()->logger->error('AliyunOss \ listObjects', $e->getMessage());
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # listObjects', $e->getMessage());
+
             return false;
         }
     }
 
     public function deleteObject($bucket, $object)
     {
-        try{
+        $di = \PhalApi\DI();
+        if (!$this->client->doesBucketExist($bucket)) {
+            $di->logger->error('AliyunOss # deleteObject # Bucket not exists', $bucket);
+
+            return false;
+        }
+        if (!$this->client->doesObjectExist($bucket, $object)) {
+            $di->logger->error('AliyunOss # deleteObject # file not exists', $object);
+
+            return true;
+        }
+        try {
             $res = $this->client->deleteObject($bucket, $object);
+
             return $res;
-        } catch(OssException $e) {
-            \PhalApi\DI()->logger->error('AliyunOss \ deleteObject', $e->getMessage());
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # deleteObject', $e->getMessage());
+
             return false;
         }
     }
 
     public function deleteObjects($bucket, $objects)
     {
-        try{
+        $di = \PhalApi\DI();
+        try {
             $res = $this->client->deleteObjects($bucket, $objects);
+
             return $res;
-        } catch(OssException $e) {
-            \PhalApi\DI()->logger->error('AliyunOss \ deleteObjects', $e->getMessage());
+        } catch (OssException $e) {
+            $di->logger->error('AliyunOss # deleteObjects', $e->getMessage());
+
             return false;
         }
     }
